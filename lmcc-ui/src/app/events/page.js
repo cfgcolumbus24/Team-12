94ddf03143e6fb14935b4af0be6d3f98883a8833
OwensJ24 <xowens8@gmail.com>
2024-11-01 18:46:30 -0400

    added Forum Button

--- lmcc-ui/src/app/events/page.js
+++ lmcc-ui/src/app/events/page.js
@@ -1,9 +1,13 @@
+import ForumButton from '@/components/ForumButton';
 import EventsCard from "@/components/EventsCard";
 
 export default function Events() {
   return (
     <div>
-      <EventsCard />
+
+
+     
+
     </div>
   );
 }
